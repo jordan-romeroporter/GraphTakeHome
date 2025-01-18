a823e30f1a5a26d49d7c9fd73b053f2ef26f55a5
jordan-romeroporter <jordan.romeroporter@gmail.com>
2025-01-18 16:00:55 -0700

    Setting up project

---
+++ GraphTakeHome/GraphTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphTakeHomeApp.swift
+//  GraphTakeHome
+//
+//  Created by Jordan Romero Porter on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphTakeHomeTests/GraphTakeHomeTests.swift
@@ -0,0 +1,16 @@
+//
+//  GraphTakeHomeTests.swift
+//  GraphTakeHomeTests
+//
+//  Created by Jordan Romero Porter on 1/18/25.
+//
+
+import Testing
+
+struct GraphTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
